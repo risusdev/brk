4008a140cc535d4bc2befb6cfe37ead98ebcb65e
Glitch (rural-internal-shear) <none>
2021-06-05 17:30:35 +0000

    🐬🐣 Checkpoint ./komutlar/yardım.js:32542478/171

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -2,8 +2,8 @@ const discord = require('discord.js');
 exports.run = async(client, message, args) => {
     const embed = new discord.MessageEmbed()
     .setTitle('Pearlys Yardım Menüsü')
-    .addField('p!kullanıcı', '<:kullanici:850777538745729036> Herkesin Kullanabiecegi Komutların Menüsü :tada:', true)
-    .addField('-yardım', 'yardım menüsü', true)
+    .addField('p!kullanıcı', '<:kullanici:850777538745729036> **Herkesin Kullanabilecegi Komutların Menüsü**', true)
+    .addField('p!yardım', '<:mod:850788085511618600> **Sunucudaki Yetkililerin Kullanabilecegi Komutlar Menüsünü**', true)
     .addField('-yardım', 'yardım menüsü', true)
     .addField('-yardım', 'yardım menüsü', true)
     .addField('-yardım', 'yardım menüsü', true)
