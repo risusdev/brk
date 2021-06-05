c4a01de39b2d0014db88cbedcbee7c177a68d815
Glitch (rural-internal-shear) <none>
2021-06-05 13:17:22 +0000

    🎩🎪 Checkpoint ./events/ready.js:32542478/3152

--- events/ready.js
+++ events/ready.js
@@ -8,14 +8,14 @@ module.exports = client => {
  setInterval(function() {
 }, 8000);
   var msgArray = [
-"DELİDİĞİNİZ",
-"DELİDİĞİNİZ",
-"DİLEDİĞİNİZ",
+"Pearlys Bot Yeniden",
+"p!yardım & p!moderasyon",
+"İstek - Öneri - Şikayet İÇin Destek Sunucuna gelmeyi Unutmayın [p!öneri]",
  ];
 
  setInterval(() => {
   var rastgeleOyun = Math.floor(Math.random() * msgArray.length);
-  client.user.setActivity(`${msgArray[rastgeleOyun]}`, { type: 'WATCH' ,  url: 'https://www.twitch.tv' })
-}, 5000);
+  client.user.setActivity(`${msgArray[rastgeleOyun]}`, { type: 'WATCH' ,  url: '' })
+}, 1000);
     console.log(`Pearlys Aktif Giriş yapıldı!`);
 }
