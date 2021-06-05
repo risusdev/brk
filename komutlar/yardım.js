b8ade27b1121973751a0c9ba5a8f85b72f2d4eed
Glitch (rural-internal-shear) <none>
2021-06-05 19:15:38 +0000

    🐁🍲 Checkpoint ./komutlar/yardım.js:32542478/224

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,9 +6,9 @@ exports.run = async(client, message, args) => {
     .addField('p!yardım', '<:mod:850788085511618600> **Moderasyon yardım menüsünü gösterir.**', true)
     .addField('p!davetsistemi', '<:davet:850789805679181844> **Davet sistemi yardım menüsünü gösterir.**', true)
     .addField('p!kayıtsistemi', '<a:kayit:850809778418221126> **Kayıt sistemi yardım menüsünü gösterir.**', true)
-    .addField('p!korumasistemi', '<a:guard~1:850810781012066415> **Koruma sistemi yardım menüsünü gösterir.**', true)
-    .addField('-yardım', 'yardım menüsü', true)
-    .addField('-yardım', 'yardım menüsü', true)
+    .addField('p!korumasistemi', '<a:guard1:850810781012066415> **Koruma sistemi yardım menüsünü gösterir.**', true)
+    .addField('p!logosistemi', '<a:logo:850814040665882664> **Logo sistemi yardım menüsünü gösterir.**', true)
+    .addField('p!botlistsistemi', '<:botlist:850815022531870731> **Bot list sistemi yardım menüsünü gösterir.**', true)
     .addField('-yardım', 'yardım menüsü', true)
     .addField('-yardım', 'yardım menüsü', true)
 
