199ca1db12445657bf77276aeea74e809a7d928c
Glitch (rural-internal-shear) <none>
2021-06-05 19:27:40 +0000

    🍠💄 Checkpoint ./komutlar/yardım.js:32542478/1732

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,7 +1,11 @@
 const discord = require('discord.js');
 exports.run = async(client, message, args) => {
     const embed = new discord.MessageEmbed()
-    .setTitle('Pearlys Yardım Menüsü')
+.setAuthor(`Pearlys Yardım Menüsü`, client.user.avatarURL())
+.setColor("0x36393F") 
+  .setDescription(`**Selam Knk <@${message.author.id}> **
+**İlk Öncelikle Beni Kullandıgın İçin Teşekkür Ederim **
+**Komutlarım Aşşadadır ** <a:assagiok:850817439783583834>`)   
     .addField('p!kullanıcı', '<:kullanici:850777538745729036> **Kullanıcı yardım menüsünü gösterir.**', true)
     .addField('p!yardım', '<:mod:850788085511618600> **Moderasyon yardım menüsünü gösterir.**', true)
     .addField('p!davetsistemi', '<:davet:850789805679181844> **Davet sistemi yardım menüsünü gösterir.**', true)
@@ -9,9 +13,11 @@ exports.run = async(client, message, args) => {
     .addField('p!korumasistemi', '<a:guard1:850810781012066415> **Koruma sistemi yardım menüsünü gösterir.**', true)
     .addField('p!logosistemi', '<a:logo:850814040665882664> **Logo sistemi yardım menüsünü gösterir.**', true)
     .addField('p!botlistsistemi', '<:botlist:850815022531870731> **Bot list sistemi yardım menüsünü gösterir.**', true)
+    .addField('p!öneri', '<a:siyahalev:850818052308467722> **Botun Sahibine Bo', true)
     .addField('-yardım', 'yardım menüsü', true)
-    .addField('-yardım', 'yardım menüsü', true)
-
+.setImage(`https://cdn.discordapp.com/attachments/850789768651603978/850815304989016064/standard.gif`)
+.setFooter("Pearlys", message.author.avatarURL())
+.setTimestamp()
 message.channel.send(embed)
 };
 exports.conf = {
