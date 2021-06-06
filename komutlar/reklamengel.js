1a1cdeaec5044ef88a8a4210fbfbc4dabb133012
Glitch (rural-internal-shear) <none>
2021-06-06 18:59:48 +0000

    🛋🦃 Checkpoint ./komutlar/reklamengel.js:32542478/150

--- komutlar/reklamengel.js
+++ komutlar/reklamengel.js
@@ -3,12 +3,12 @@ const db = require("quick.db");
  
 exports.run = async(client, message, args) => {
   if (!args[0]) return message.channel.send(`Lütfen bir seçenek belirt! (aç/kapat)`)
-  if (!message.member.hasPermission('ADMİNOS')) return message.channel.send('<@${message.author.id}> **Bu Komutu Kullanabilmek İçin Yetkiniz Bulunmamaktadır !')
+  if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("**Bu Komutu Kullanabilmek İçin Yetkiniz Bulunmamaktadır !**")
  
   if (args[0] === 'aç') {
     
     db.set(`reklamengel_${message.guild.id}`, 'aktif')
-    message.channel.send(`Reklam engel başarı ile açıldı!`)
+    message.channel.send(`<:mod:850788085511618600> | **Reklam engel başarı ile açıldı! <:evet:851169108262912000> **`)
  
   }
 
