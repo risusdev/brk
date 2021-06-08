06d96815eb3367f1bab9392a49fb0244ff8be58b
Glitch (rural-internal-shear) <none>
2021-06-08 07:06:50 +0000

    🎄👔 Checkpoint ./komutlar/yardım-korumasistemi.js:32542478/5294 ./komutlar/yardım-kayıtsistemi.js:32542478/504

--- komutlar/yardım-kayıtsistemi.js
+++ komutlar/yardım-kayıtsistemi.js
@@ -8,20 +8,20 @@ exports.run = (client, message) => {
  const WestraEmbed = new WestraDiscord.MessageEmbed()
   .setColor(0x36393F)
  .setAuthor(`${client.user.username} | Kayıt Sistemi Yardım Menüsü`)
- .setDescription(`<a:HareketliHype:822382020725309465> **${WestraPrefix}alınacak-rol @rol** \n-> Kayıt edilen kişiden alınacak rolü ayarlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}alınacak-rol sıfırla** \n-> Kayıt edilen kişiden alınacak rolü sıfırlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kayıt-kanal #kanal** \n-> Kayıtın yapılacağı kanalı belirlersiniz.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kayıt-kanal sıfırla** \n-> Kayıtın yapılacağı kanalı sıfırlarsınız.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kayıt-hg #kanal** \n-> Kayıt hoş geldin kanalını ayarlarsınız.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kayıt-hg sıfırla** \n-> Kayıt hoş geldin kanalını sıfırlarsınız.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kayıt-yetkili @rol** \n-> Kayıt edebilecek yetkiyi ayarlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kayıt-yetkili sıfırla** \n-> Kayıt edebilecek yetkiyi sıfırlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}erkek-rol @rol** \n-> Kayıt edilince verilecek erkek rolü ayarlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}erkek-rol sıfırla** \n-> Kayıt edilince verilecek erkek rolünü sıfırlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kız-rol @rol** \n-> Kayıt edilince verilecek kız rolü ayarlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kız-rol sıfırla** \n-> Kayıt edilince verilecek kız rolünü sıfırlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}erkek @kullanıcı isim yaş** \n-> Erkekleri kayıt etmeye yarar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kız @kullanıcı isim yaş** \n-> Kızları kayıt etmeye yarar.
+ .setDescription(` **${WestraPrefix}alınacak-rol @rol** \n-> Kayıt edilen kişiden alınacak rolü ayarlar.
+ **${WestraPrefix}alınacak-rol sıfırla** \n-> Kayıt edilen kişiden alınacak rolü sıfırlar.
+ **${WestraPrefix}kayıt-kanal #kanal** \n-> Kayıtın yapılacağı kanalı belirlersiniz.
+ **${WestraPrefix}kayıt-kanal sıfırla** \n-> Kayıtın yapılacağı kanalı sıfırlarsınız.
+ **${WestraPrefix}kayıt-hg #kanal** \n-> Kayıt hoş geldin kanalını ayarlarsınız.
+ **${WestraPrefix}kayıt-hg sıfırla** \n-> Kayıt hoş geldin kanalını sıfırlarsınız.
+ **${WestraPrefix}kayıt-yetkili @rol** \n-> Kayıt edebilecek yetkiyi ayarlar.
+ **${WestraPrefix}kayıt-yetkili sıfırla** \n-> Kayıt edebilecek yetkiyi sıfırlar.
+ **${WestraPrefix}erkek-rol @rol** \n-> Kayıt edilince verilecek erkek rolü ayarlar.
+ **${WestraPrefix}erkek-rol sıfırla** \n-> Kayıt edilince verilecek erkek rolünü sıfırlar.
+ **${WestraPrefix}kız-rol @rol** \n-> Kayıt edilince verilecek kız rolü ayarlar.
+ **${WestraPrefix}kız-rol sıfırla** \n-> Kayıt edilince verilecek kız rolünü sıfırlar.
+ **${WestraPrefix}erkek @kullanıcı isim yaş** \n-> Erkekleri kayıt etmeye yarar.
+ **${WestraPrefix}kız @kullanıcı isim yaş** \n-> Kızları kayıt etmeye yarar.
 `)
  .setFooter(`Pearlys`)
  .setTimestamp()
--- komutlar/yardım-korumasistemi.js
+++ komutlar/yardım-korumasistemi.js
@@ -1,37 +1,38 @@
-const WestraDiscord = require('discord.js');
-const WestraClient = new WestraDiscord.Client();
-const ayar = require('../config.json');
-let WestraPrefix = ayar.prefix
-let WestraGif = ayar.giflink
+const discord = require('discord.js');
+exports.run = async(client, message, args) => {
+    const embed = new discord.MessageEmbed()
+.setAuthor(`Pearlys Koruma Sistemi Yardım Menüsü`, client.user.avatarURL())
+.setColor("#09f3f3") 
+  .setTitle(`Sanırım Kullanıcı Komutlarımı Merak Ettin <a:assagiok:850817439783583834>`)   
+     .setDescription(`
+<a:guard1:850810781012066415> **p!ban-koruma #kanal**
+> **Ban koruma sistemini açar.**
+<a:guard1:850810781012066415> **p!kanal-koruma #kanal**
+> **Kanal koruma sistemini açar.**
+<a:guard1:850810781012066415> **p!rol-koruma #kanal**
+> **Rol koruma sistemini açar.**
+<a:guard1:850810781012066415> **p!spam-koruma**
+> ** Spam koruma sistemini açar.**
 
-exports.run = (client, message) => {
- const WestraEmbed = new WestraDiscord.MessageEmbed()
-  .setColor(0x36393F)
- .setAuthor(`${client.user.username} | Koruma Sistemi Yardım Menüsü`)
- .setDescription(`
-<a:HareketliHype:822382020725309465> **${WestraPrefix}ban-koruma #kanal** \n-> Ban koruma sistemini açar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}ban-koruma-sıfırla** \n-> Ban koruma sistemini sıfırlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kanal-koruma #kanal** \n-> Kanal koruma sistemini açar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}kanal-koruma-sıfırla** \n-> Kanal koruma sistemini sıfırlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}rol-koruma #kanal** \n-> Rol koruma sistemini açar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}rol-koruma-sıfırla** \n-> Rol koruma sistemini sıfırlar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}spam-koruma** \n-> Spam koruma sistemini açar.
-<a:HareketliHype:822382020725309465> **${WestraPrefix}spam-koruma-kapat** \n-> Spam koruma sistemini kapatır.`)
- .setFooter(`Pearlys`)
- .setTimestamp()
- .setImage(WestraGif)
- message.channel.send(WestraEmbed)
-}
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  kategori: "Bot",
-  permLevel: 0
+<a:assagiok:850817439783583834> **__Sıfırlamak İçin__** <a:assagiok:850817439783583834>
+
+<a:guard1:850810781012066415> **__Bir Komutu Sıfırlamak İçin p!<komut>-sıfırla Örnegin : p!rol-koruma-sıfırla Şeklinde Sıfırlıyabilirsiniz !__**
+> **Bi Koruma Sistemini Nasıl Sıfırlayabileceginiz Anlatılır.**
+`)   
+.setImage(`https://cdn.discordapp.com/attachments/850789768651603978/850815304989016064/standard.gif`)
+.setFooter("Pearlys", message.author.avatarURL())
+.setTimestamp()
+message.channel.send(embed)
 };
+exports.conf = {
+enabled: true,
+guildOnly: false,
+permLevel: 0,
+aliases: ['koruma']
 
+
+
+};
 exports.help = {
-  name: 'korumasistemi',
-  description: 'Bot ile ilgili bilgi verir.',
-  usage: 'bilgi'
+name: "koruma-sistemi"
 };
